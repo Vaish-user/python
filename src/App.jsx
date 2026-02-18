cc80d1dfd7b14dd075fe86b83c3d74f6c5501c70
VAISH-user <vaishnavivelayutham05pnk.com>
2026-02-18 15:08:55 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button>Click me</button>
+      <home name="hackgenic"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/operators.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const operators = () => {
+  return (
+    <div>operators</div>
+  )
+}
+
+export default operators
